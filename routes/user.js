212d3a44c934657906d602073291f508a075cada
saegeullee <lemix777@naver.com>
2019-11-20 21:12:16 +0900

    Implemented user authorization functionality
    Implementations
    1. Implemented user authorization handler loginRequired
    2. Updated user signup error conditions

--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,6 @@ const authController = require("controllers/auth");
 
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
+// router.post("/test", authController.loginRequired);
 
 module.exports = router;
