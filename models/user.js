f4001cf00a6005a88c61f55977622a06915be550
Daniel Yoo <onikss793@gmail.com>
2019-11-21 15:32:37 +0900

    Google Social Login
    1. In UserSchema, field password option required changed to none,
      because of social login
    2. In auth/controllers, get id_token from frontend and checkout User
    Database. If already exist, generate token and if not, nake new User and
    generate token
    3. In services/auth, function getTokenGoogle, the standard is sub which
    converted to { provider: socialId }. With provider find existing User.

--- models/user.js
+++ models/user.js
@@ -2,26 +2,26 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema(
-    {
-        name: { type: String, required: true },
-        nickname: { type: String, unique: [true, "nickname already exists"] },
-        email: { type: String, unique: [true, "email already exists"] },
-        password: { type: String, required: true },
-        address: String,
-        detailAddress: String,
-        provider: String,
-        photoUrl: String,
-        phone: String,
-        orderCount: Number,
-        creater: { type: Boolean, default: false },
-        recentViewedProductIds: [{ type: Schema.Types.ObjectId, ref: "Product" }],
-        myProduct: { type: Schema.Types.ObjectId, ref: "Product" },
-        purchasedProducts: [{ type: Schema.Types.ObjectId, ref: "Product" }],
-        favoriteProducts: [{ type: Schema.Types.ObjectId, ref: "Product" }]
-    },
-    {
-        timestamps: true
-    }
+  {
+    name: { type: String, required: true },
+    nickname: { type: String, unique: [true, "nickname already exists"] },
+    email: { type: String, unique: [true, "email already exists"] },
+    password: { type: String },
+    address: String,
+    detailAddress: String,
+    provider: Object,
+    photoUrl: String,
+    phone: String,
+    orderCount: Number,
+    creater: { type: Boolean, default: false },
+    recentViewedProductIds: [{ type: Schema.Types.ObjectId, ref: "Product" }],
+    myProduct: { type: Schema.Types.ObjectId, ref: "Product" },
+    purchasedProducts: [{ type: Schema.Types.ObjectId, ref: "Product" }],
+    favoriteProducts: [{ type: Schema.Types.ObjectId, ref: "Product" }]
+  },
+  {
+    timestamps: true
+  }
 );
 
 module.exports = mongoose.model("User", UserSchema);
